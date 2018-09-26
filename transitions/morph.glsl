31041178541abce7b44fb14cc827a57de1b03476
bastien <bastien.cornier@gmail.com>
2018-09-26 16:06:58 +0200

    Morph with direction

--- transitions/morph.glsl
+++ transitions/morph.glsl
@@ -9,8 +9,10 @@ vec4 transition(vec2 p) {
   vec2 oa = (((ca.rg+ca.b)*0.5)*2.0-1.0);
   vec2 ob = (((cb.rg+cb.b)*0.5)*2.0-1.0);
   vec2 oc = mix(oa,ob,0.5)*strength;
+  float tr0 = progress*0.2;
+  float tr1 = (1.0 - progress)*0.2;
   
   float w0 = progress;
   float w1 = 1.0-w0;
-  return mix(getFromColor(p+oc*w0), getToColor(p-oc*w1), progress);
+  return mix(getFromColor(vec2(p.x, p.y+tr0)+oc*w0), getToColor(vec2(p.x, p.y+tr1)-oc*w1), progress);
 }
